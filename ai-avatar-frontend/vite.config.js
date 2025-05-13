e23408c0102c696ddf872e3709a592942a6b8246
Charangitt <160092118+Charangitt@users.noreply.github.com>
2025-05-13 23:13:18 +0530

    Update vite.config.js

--- ai-avatar-frontend/vite.config.js
+++ ai-avatar-frontend/vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '#minpath': path.resolve(__dirname, 'node_modules/vfile/lib/minpath.js'),
+    },
+  },
 })
